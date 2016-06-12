1f68c84c0c3c34497d57f3b2988be2f438fbcd6a
Andrew Madsen <andrew@mixedinkey.com>
2016-06-12 10:38:45 -0600

    Removed unused methods in AppDelegate.

--- MIDI File Explorer/AppDelegate.m
+++ MIDI File Explorer/AppDelegate.m
@@ -14,12 +14,4 @@
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	// Insert code here to initialize your application
-}
-
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-	// Insert code here to tear down your application
-}
-
 @end
